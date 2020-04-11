a963e72f3506599aa6b111c03c2c6d2eef6fdbba
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-04-11 03:45:40 -0300

    Funcionalidades terminadas
    Adicionei todas as funcionalidades básicas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,53 @@ function criaCalculadora() {
         display: document.querySelector('.display'),
         inicia() {
             this.cliqueBotoes();
+            this.pressionaEnter();
         },
-        cliqueBotoes() {
-            document.addEventListener('click', function(e) {
-                const el = e.target;
-                if (el.classList.contains('btn-num')) {
-                    this.btnParaDisplay();
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode == 13) {
+                    this.realizaConta();
                 }
             })
         },
-        btnParaDisplay() {
-            
+        limparDisplay() {
+            this.display.value = '';
+        },
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+        realizaConta() {
+            let conta = this.display.value;
+            conta = conta.replace('×', '*');
+            conta = conta.replace('÷', '/');
+            try {
+                conta = eval(conta);
+                if (!conta) {
+                    alert('Conta inválida');
+                    return;
+                }
+                this.display.value = String(conta);
+            } catch (e) {
+                alert('Conta inválida');
+                return;
+            }
+        },
+        cliqueBotoes() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                } else if (el.classList.contains('btn-limpa')) {
+                    this.limparDisplay();
+                } else if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                } else if (el.classList.contains('btn-igual')) {
+                    this.realizaConta();
+                }
+            })
+        },
+        btnParaDisplay(valor) {
+            this.display.value += valor;
         }
     };
 }
